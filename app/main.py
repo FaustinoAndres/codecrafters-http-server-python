48a7db780269976b2b3dd5050e7f99e11e2e404f
Faustino Correa <faustino.correa.m@gmail.com>
2024-05-01 12:57:06 -0400

    [parse headers]: Parse User-Agent

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ class Request:
     path: str
     protocol: str
     host: str
+    user_agent: str
 
 
 def parse_request(data: bytes) -> Request:
@@ -15,20 +16,30 @@ def parse_request(data: bytes) -> Request:
     http_request = data.split('\r\n')
     header = http_request[0].split(' ')
     host = http_request[1].lower().lstrip('host: ')
+    user_agent = http_request[2].lower().lstrip('user-agent: ')
+
     return Request(
         method=header[0],
         path=header[1],
         protocol=header[2],
-        host=host
+        host=host,
+        user_agent=user_agent
     )
 
 
-def send_response(conn: socket.socket, path: str):
+def send_response(conn: socket.socket, request: Request):
+
+    path = request.path
+    user_agent = request.user_agent
 
     if path == "/":
         status = 200
         conn.send(bytes(f"HTTP/1.1 {status} OK\r\n\r\n", "utf-8"))
-    if '/echo/' in path:
+    elif path == "/user-agent":
+        status = 200
+        text = user_agent
+        conn.send(bytes(f"HTTP/1.1 {status} OK\r\nContent-Type: text/plain\r\nContent-Length: {len(text)}\r\n\r\n{text}\r\n", "utf-8"))
+    elif '/echo/' in path:
         status = 200
         text = path.replace('/echo/', '')
         conn.send(bytes(f"HTTP/1.1 {status} OK\r\nContent-Type: text/plain\r\nContent-Length: {len(text)}\r\n\r\n{text}\r\n", "utf-8"))
@@ -44,7 +55,7 @@ def main():
 
     data = connection.recv(1024)
     req = parse_request(data)
-    send_response(connection, req.path)
+    send_response(connection, req)
     connection.close()
     server_socket.close()
 
